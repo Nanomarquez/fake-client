016575f1c7d43a321c5e20c39c0928c7cdeb165a
Nanomarquez <nanomarqueztres@gmail.com>
2022-11-25 01:35:42 -0300

    axios

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import axios from 'axios'
 if(import.meta.env.MODE === 'development'){
   axios.defaults.baseURL = 'http://localhost:3001';
 }
